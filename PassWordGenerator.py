b446fc52117d56a09e85c92ca7f5de623ff5b9d8
Rutuja1801 <rutuvw18@gmail.com>
2023-11-16 15:15:53 +0530

    Add paragraph

---
+++ PassWordGenerator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_password(len):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(len))
+    return password
+
+def main():
+    print("Generator")
+
+    len = int(input("Enter length of password: "))
+
+    password = generate_password(len)
+
+    print("Generated Password: ", password)
+
+if __name__ == "__main__":
+    main()
